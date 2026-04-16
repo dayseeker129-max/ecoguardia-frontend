a1adeefb70665aaee030e531a854b1c31c846841
dayseeker129-max <dayseeker129@gmail.com>
2026-04-16 14:06:35 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 // Esto crea un acceso directo a tu servidor
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "hhttps://ecoguardia-backend.onrender.com/api"
 });
 
 // ESTO ES LO QUE AGREGASTE (El interceptor)
